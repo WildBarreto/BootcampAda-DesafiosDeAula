9c373d58009843a079a5e02ca646089f3188c658
WildBarreto <wildsbsrreto@gmail.com>
2023-11-07 16:47:19 -0300

    Desafio da aula 3

---
+++ Logica-de-programação/Desafio-aula-3/index.js
@@ -0,0 +1,26 @@
+const partners = {
+  19660156627897: "Fernanda Santos",
+  23998058019370: " Rafael Souza",
+  92291338611: "Maria Silva",
+  55443795656: "Maria Souza",
+  77743761456: "Ana Costa",
+  47202302326: "Maria Ferreira",
+  58017232567: "Sofia Costa",
+  16733009491247: "Lucas Silva",
+  63351859919: "Rafael Souza",
+  84297701780: "Carlos Oliveira",
+}
+const groupedPartners = {
+  PF: [],
+  PJ: [],
+}
+
+for (let id in partners) {
+  if (id.length === 11) {
+    groupedPartners.PF.push({ partnersId: id, nome: partners[id] });
+
+  } else {
+    groupedPartners.PJ.push({ partnersId: id, nome: partners[id] });
+  }
+}
+console.log(groupedPartners)
