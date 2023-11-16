ad1901ec3b9459a7ab9a9f73095c8070e8469f11
WildBarreto <wildsbsrreto@gmail.com>
2023-11-16 20:18:32 -0300

    Ajustes

--- Logica-de-programação/Desafio-aula-5/index.js
+++ Logica-de-programação/Desafio-aula-5/index.js
@@ -12,11 +12,11 @@ console.log("Sua altura é: " + altura);
 console.log("Seu IMC é: " + imc)
 
 if (imc < 18.5) {
-  alert("Seu IMC é: " + imc.toFixed(4) + " você está abaixo do peso, seu peso ideal é: " + pesoIdeal + "KG")
+  alert(`Seu IMC é: ${imc.toFixed(4)}, você está abaixo do peso, seu peso ideal é: ${pesoIdeal}KG`);
 } else if (imc > 18.5 && imc < 24.9) {
-  alert("Seu IMC é: " + imc.toFixed(4) + ", seu peso está normal, seu peso ideal é: " + pesoIdeal + "KG");
+  alert(`Seu IMC é: ${imc.toFixed(4)}, seu peso está normal, seu peso ideal é: ${pesoIdeal}KG`);
 } else if (imc >= 25 && imc < 29.9) {
-  alert("Seu IMC é: " + imc.toFixed(4) + ", você está com sobre peso seu peso ideal é: " + pesoIdeal + "KG")
+  alert(`Seu IMC é: ${imc.toFixed(4)}, você está com sobre peso seu peso ideal é: ${pesoIdeal}KG`);
 } else {
-  alert("Seu IMC é: " + imc.toFixed(4) + ", você está com obesidade seu peso ideal é: " + pesoIdeal + "KG");
+  alert(`Seu IMC é: ${imc.toFixed(4)}, você está com obesidade seu peso ideal é: ${pesoIdeal}KG`);
 }
